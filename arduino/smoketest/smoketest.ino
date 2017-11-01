94451e2479f100f4be918bfe44e42e1daa392fe2
Thilo Fischer <thilo.fischer@ksti.de>
2017-11-01 23:13:04 +0100

    Arduino smoketest: refactor

--- arduino/smoketest/smoketest.ino
+++ arduino/smoketest/smoketest.ino
@@ -1,4 +1,4 @@
-// Turn off LED when switch connects pin 4 to GND.
+// Turn on LED when switch connects pin 4 to GND.
 
 // Original Arduino Micro has built-in LED connected to digital pin 13 (LED_BUILTIN).
 // The cheap Arduino Micro compatible board I use most of the times ("Pro-Micro-v11")
@@ -6,23 +6,37 @@
 #define LED_BUILTIN_ALT 17
 
 // We will need pins 2 und 3 for I2C/TWI later on => use pin 4 for switch
-#define INPUT_PIN 4
+#define SWITCH_PIN 4
 
-void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(LED_BUILTIN_ALT, OUTPUT);
-
-  pinMode(INPUT_PIN, INPUT_PULLUP);
-}
-
-void loop() {
-
-  if (digitalRead(INPUT_PIN) == HIGH) {
+void light_led(bool active) {
+  if (active) {
     digitalWrite(LED_BUILTIN, LOW);
     digitalWrite(LED_BUILTIN_ALT, LOW);
   } else {
     digitalWrite(LED_BUILTIN, HIGH);
     digitalWrite(LED_BUILTIN_ALT, HIGH);
+  }
+}
+
+bool switch_pressed() {
+  return digitalRead(SWITCH_PIN) == LOW;
+}
+
+void setup() {
+  // LED pins (original and alternative pin)
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN_ALT, OUTPUT);
+
+  // switch to GND pin
+  pinMode(SWITCH_PIN, INPUT_PULLUP);
+}
+
+void loop() {
+
+  if (switch_pressed()) {
+    light_led(true);
+  } else {
+    light_led(false);
   }
 
    delay(50);
